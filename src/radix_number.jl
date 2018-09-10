6f8188a760fb176b5f49683ff94a8817c29ba170
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-09-11 01:05:11 +0200

    Add divrem() for radix numbers

--- src/radix_number.jl
+++ src/radix_number.jl
@@ -1,6 +1,6 @@
 import Base:
     +, -, *, ^, ==, >, >=, <, <=,
-    convert, show, zero, setindex, length, div, divrem, isodd, one
+    convert, show, zero, setindex, getindex, length, div, divrem, isodd, one
 
 
 struct RadixNumber{N, T <: Unsigned}
@@ -48,6 +48,8 @@ end
 @inline function setindex(x::RadixNumber{N, T}, v::T, i::Integer) where N where T
     RadixNumber(setindex(x.value, v, i))
 end
+
+@inline getindex(x::RadixNumber{N, T}, i::Integer) where N where T = x.value[i]
 
 
 @inline function -(x::RadixNumber{N, T}, y::RadixNumber{N, T}) where N where T
@@ -162,10 +164,10 @@ end
 @inline function *(x::RadixNumber{N, T}, y::RadixNumber{N, T}) where N where T
     # TODO: to protect from timing attacks we can assume n == t == N
     # This will also allow us to use a generated function and may turn out to be faster...
-    n = _most_significant_digit(x)
-    t = _most_significant_digit(y)
+    #n = _most_significant_digit(x) - 1
+    t = _most_significant_digit(y) - 1
     w = zero(RadixNumber{N, T})
-    for i in 1:t
+    for i in 1:t+1
         c = zero(T)
         hi = zero(T)
         for j in 1:N+1-i # i + j - 1 <= N -> j <= N + 1 - i
@@ -210,14 +212,150 @@ function ^(x::RadixNumber{N, T}, y::Integer) where N where T
 end
 
 
+function _ge_shift(x::RadixNumber{N, T}, y::RadixNumber{N, T}, y_shift::Integer) where N where T
+    # true if x >= y * b^y_shift
+    for i in N:-1:1+y_shift
+        if x.value[i] == y.value[i - y_shift]
+            continue
+        end
+        return x.value[i] > y.value[i - y_shift]
+    end
+    true
+end
+
+
+function _shift_digits(x::RadixNumber{N, T}, shift::Integer) where N where T
+    # x -> x * b^shift
+    res = zero(RadixNumber{N, T})
+    for i in 1:shift
+        res = setindex(res, zero(T), i)
+    end
+    for i in shift+1:N
+        res = setindex(res, x[i - shift], i)
+    end
+    res
+end
+
+
+function divhilo(x0::T, x1::T, y::T) where T <: Unsigned
+    # Assuming 0 <= x1 < y, otherwise the overflow will be ignored
+    res = zero(T)
+
+    a, alpha = divrem(typemax(T), y)
+    alpha += one(T)
+    if alpha == 0
+        a += one(T)
+    end
+    c, gamma = divrem(x0, y)
+    res += x1 * a + c
+
+    lo, hi = mulhilo(x1, alpha)
+    lo, hi = addhilo(lo, hi, gamma)
+    if hi == 0
+        res + div(lo, y)
+    else
+        res + divhilo(lo, hi, y)
+    end
+end
+
+
+# Calculates y * (x0 + x1 * b) -> r0, r1, r2
+function _mul_1d_2d(x0::T, x1::T, y::T) where T <: Unsigned
+    # Adapted from the generic multiplication for radix integers
+    lo, hi = mulhilo(x0, y)
+    w0 = lo
+    c = hi
+
+    lo, hi = mulhilo(x1, y)
+    lo, hi = addhilo(lo, hi, c)
+    w1 = lo
+    w2 = hi
+
+    (w0, w1, w2)
+end
+
+
+# Calculates x - y * z
+function _sub_mul(x::RadixNumber{N, T}, y::T, z::RadixNumber{N, T}) where N where T
+    hi_carry1 = false
+    hi_carry2 = false
+    hi_carry3 = false
+    hi = zero(T)
+    out = zero(RadixNumber{N, T})
+    for j in 1:N
+        lo, new_hi = mulhilo(y, z.value[j])
+
+        r, new_hi_carry1 = _subc(x.value[j], lo)
+        r, new_hi_carry2 = _subc(r, hi)
+        r, new_hi_carry3 = _subc(r, T(hi_carry1 + hi_carry2 + hi_carry3))
+
+        out = setindex(out, r, j)
+        hi = new_hi
+        hi_carry1 = new_hi_carry1
+        hi_carry2 = new_hi_carry2
+        hi_carry3 = new_hi_carry3
+    end
+
+    out_hi, c1 = _subc(zero(T), hi)
+    out_hi, c2 = _subc(out_hi, T(hi_carry1 + hi_carry2 + hi_carry3))
+    out, c1 || c2 # TODO: it seems that we can have at most 1 carried over to the n+2-th digit
+end
+
+
 function divrem(x::RadixNumber{N, T}, y::RadixNumber{N, T}) where N where T
-    d, r = divrem(convert(BigInt, x), convert(BigInt, y))
-    convert(RadixNumber{N, T}, d), convert(RadixNumber{N, T}, r)
+    n = _most_significant_digit(x) - 1
+    t = _most_significant_digit(y) - 1
+
+    if n < t
+        return zero(RadixNumber{N, T}), x
+    end
+
+    q = zero(RadixNumber{N, T})
+    r = zero(RadixNumber{N, T})
+
+    if t == 0 && n == 0
+        q1, r1 = divrem(x[1], y[1])
+        return setindex(q, q1, 1), setindex(r, r1, 1)
+    end
+
+    # TODO: can be replaced by `<` and `shift_digits`
+    while _ge_shift(x, y, n - t)
+        q = setindex(q, q[n - t + 1] + one(T), n - t + 1)
+        x = x - _shift_digits(y, n - t)
+    end
+
+    if n == 1 && t == 0
+        r = divhilo(x[1], x[2], y[1])
+        lo, hi = addhilo(q[1], q[2], r)
+        q = q + RadixNumber((lo, hi))
+
+        r = _sub_mul(x, r, RadixNumber((y[1], zero(T))))
+        return q, r
+    end
+
+    for i in n:-1:t+1
+        if x[i] == y[t]
+            q = setindex(q, typemax(T), i - t - 1 + 1)
+        else
+            q = setindex(q, divhilo(x[i-1+1], x[i+1], y[t+1]), i - t - 1 + 1)
+        end
+
+        while RadixNumber(_mul_1d_2d(y[t-1+1], y[t+1], q[i-t-1+1])) > RadixNumber((x[i-2+1], x[i-1+1], x[i+1]))
+            q = setindex(q, q[i - t - 1+1] - one(T), i - t - 1+1)
+        end
+
+        x, c = _sub_mul(x, q[i-t-1+1], _shift_digits(y, i - t - 1))
+        if c
+            x = x + _shift_digits(y, i - t - 1)
+            q = setindex(q, q[i - t - 1+1] - one(T), i - t - 1+1)
+        end
+    end
+
+    (q, x)
 end
 
 
 #=
-
 @inline function mprec_mul_hilo(x::RadixNumber{N, T}, y::RadixNumber{N, T}) where N where T
     s = length(x)
     n = most_significant_digit(x)
@@ -236,68 +374,6 @@ end
         w[i + n] = hi
     end
     w
-end
-
-
-function _ge_array(x::Array{T, 1}, y::Array{T, 1}, y_shift) where T <: Unsigned
-    # true if x >= y * b^y_shift
-    n = length(y)
-    for i in n:-1:1+y_shift
-        if y[i - y_shift] < x[i]
-            return true
-        elseif y[i - y_shift] > x[i]
-            return false
-        end
-    end
-    return true
-end
-
-
-function mprec_divrem(x::Array{T, 1}, y::Array{T, 1}) where T <: Unsigned
-    s = length(x)
-    n = most_significant_digit(x)
-    t = most_significant_digit(y)
-
-    if n < t
-        return zeros(T, s), x
-    end
-
-    if t == 1
-        if n == 1
-            d = zeros(T, s)
-            r = zeros(T, s)
-            d[1], r[1] = divrem(x[1], y[1])
-            return d, r
-        else
-            error()
-        end
-    end
-
-    q = zeros(T, s)
-    r = zeros(T, s)
-
-    while ge_array(x, y, n - t)
-        q[n - t + 1] += 1
-
-        # x -= y * b^(n-1)
-        carry = false
-        for i in n:-1:1+(n-t)
-            d = y[i-(n-t)] + carry
-            carry = x[i] > d
-            x[i] -= d
-        end
-        if carry
-            x[n-t] -= 1
-        end
-    end
-
-    for i in n:-1:t+1
-        if x[i] == y[t]
-            q[i-t] = typemax(T)
-        else
-            q[i-t] =
-        end
-    end
 end
 =#
 
--- test/test_radix_number.jl
+++ test/test_radix_number.jl
@@ -4,7 +4,7 @@ using BenchmarkTools
 using Random
 
 
-using SGFHE: RadixNumber
+using SGFHE: RadixNumber, divhilo, _sub_mul
 
 
 function test_mul()
@@ -70,6 +70,129 @@ function test_sub()
 end
 
 
+function test_divhilo()
+    tp = UInt8
+
+    # regression: x0 = 66, x1 = 22, y = 191
+
+    for x1 in 0:255
+        for x0 in 0:255
+            for y in x1+1:255
+
+                ref = div(Int(x0) + Int(x1) * (typemax(tp) + 1), Int(y))
+                test = divhilo(tp(x0), tp(x1), tp(y))
+                println("x0=$x0, x1=$x1, y=$y, test=$test, ref=$ref")
+                @assert ref == test
+            end
+        end
+    end
+end
+
+
+function test_divhilo_performance()
+    tp = UInt64
+
+    x0 = rand(UInt64)
+    x1 = rand(UInt64)
+    y = rand(x1+1:typemax(UInt64))
+
+    display(@benchmark divhilo($x0, $x1, $y))
+end
+
+
+function test_sub_mul()
+
+    tp = UInt8
+    len = 4
+    rmod = BigInt(2)^(sizeof(tp) * 8 * len)
+
+    Random.seed!(123)
+
+    for i in 1:1000
+
+        xr = RadixNumber(tuple(rand(tp, len)...))
+        yr = rand(tp)
+        zr = RadixNumber(tuple(rand(tp, len)...))
+
+        x = convert(BigInt, xr)
+        y = BigInt(yr)
+        z = convert(BigInt, zr)
+
+        resr, carry = _sub_mul(xr, yr, zr)
+        test = convert(BigInt, resr)
+        ref = x - y * z
+
+        if ref < 0
+            ref_carry = true
+            ref = mod(ref, rmod)
+        else
+            ref_carry = false
+        end
+
+        #println("$x=xr, $y=y, $z=zr, ref=$(convert(RadixNumber{len, tp}, ref)), test=$resr")
+        #println("ref=$ref test=$test")
+        @assert test == ref
+        @assert carry == ref_carry
+    end
+
+end
+
+
+function test_divrem()
+    len = 2
+    tp = UInt64
+    rtp = RadixNumber{len, tp}
+
+    for i in 1:1000
+        x = BigInt(rand(UInt128(0):UInt128(2)^(sizeof(tp) * 8 * len)-1))
+        y = BigInt(rand(UInt128(0):UInt128(2)^(sizeof(tp) * 8 * len)-1))
+        d, r = divrem(x, y)
+
+        xr = convert(rtp, x)
+        yr = convert(rtp, y)
+
+        println("Starting")
+        dr, rr = divrem(xr, yr)
+
+        d_test = convert(BigInt, dr)
+        r_test = convert(BigInt, rr)
+
+        println("x=$x, y=$y, d=$dr, r=$rr")
+        println("ref: d=$(convert(rtp, d)), r=$(convert(rtp, r))")
+
+        @assert d_test == d
+        @assert r_test == r
+    end
+end
+
+
+function test_divrem_performance()
+    len = 2
+    tp = UInt64
+    rtp = RadixNumber{len, UInt8}
+
+    x = rand(UInt128(0):typemax(UInt128))
+    y = rand(UInt128(1):typemax(UInt128))
+
+    println("UInt128")
+    display(@benchmark divrem($x, $y))
+    println()
+
+    xr = convert(rtp, x)
+    yr = convert(rtp, y)
+
+    println("2 x UInt64")
+    display(@benchmark divrem($xr, $yr))
+    println()
+end
+
+
 test_mul()
 test_add()
 test_sub()
+test_divhilo()
+test_divhilo_performance()
+test_sub_mul()
+test_divrem()
+test_divrem_performance()
+

bb2226f634d416bd7fce87d207b488a7b9155172
Bogdan Opanchuk <bogdan@opanchuk.net>
2018-10-28 18:28:40 +1100

    test/performance: create an RNG only once per testcase

--- test/performance.test.jl
+++ test/performance.test.jl
@@ -31,13 +31,13 @@ for rr_type in [RRElem, RRElemMontgomery],
         use_rng in ([false, true] => ["deterministic", "random"]),
         num_type in [UInt64, MPNumber{2, UInt32}, MPNumber{4, UInt16}]
 
-    rng = Random.MersenneTwister(123)
+    rng = Random.MersenneTwister()
 
     params = Params(64; rlwe_type=num_type, rr_type=rr_type)
 
     poly_type = rr_type{num_type, params.Q}
 
-    flatten_rng = use_rng ? MersenneTwister() : nothing
+    flatten_rng = use_rng ? rng : nothing
 
     a = poly_type(rand(rng, UInt128))
     B_r = poly_type(params.B)
@@ -58,13 +58,13 @@ for rr_type in [RRElem, RRElemMontgomery],
         use_rng in ([false, true] => ["deterministic", "random"]),
         num_type in [UInt128, MPNumber{2, UInt64}, MPNumber{3, UInt32}]
 
-    rng = Random.MersenneTwister(123)
+    rng = Random.MersenneTwister()
 
     params = Params(1024; rlwe_type=num_type, rr_type=rr_type)
 
     poly_type = rr_type{num_type, params.Q}
 
-    flatten_rng = use_rng ? MersenneTwister() : nothing
+    flatten_rng = use_rng ? rng : nothing
 
     a = poly_type(rand(rng, UInt128))
     B_r = poly_type(params.B)
@@ -89,23 +89,24 @@ for     rr_type in [RRElem, RRElemMontgomery],
     l = 2
     l_val = Val(l)
 
-    rng = use_rng ? MersenneTwister() : nothing
+    rng = MersenneTwister()
+    ext_prod_rng = use_rng ? rng : nothing
 
     poly_type = rr_type{num_type, p.Q}
 
-    a = Polynomial(poly_type.(rand(Int128, p.n)), true)
-    b = Polynomial(poly_type.(rand(Int128, p.n)), true)
+    a = Polynomial(poly_type.(rand(rng, Int128, p.n)), true)
+    b = Polynomial(poly_type.(rand(rng, Int128, p.n)), true)
 
     B_m = poly_type(p.B)
     pz = Polynomial(poly_type.(zeros(Int, p.n)), true)
     G = ([pz pz; pz pz; pz pz; pz pz] .+ poly_type.([1 0; p.B 0; 0 1; 0 p.B]))
 
     base = Val(B_m)
-    a_restored, b_restored = external_product(rng, a, b, G, base, l_val)
+    a_restored, b_restored = external_product(ext_prod_rng, a, b, G, base, l_val)
     @test a == a_restored
     @test b == b_restored
 
-    trial = @benchmark external_product($rng, $a, $b, $G, $base, $l_val)
+    trial = @benchmark external_product($ext_prod_rng, $a, $b, $G, $base, $l_val)
     @test_result "ext_prod: " * benchmark_result(trial)
 end)
 
